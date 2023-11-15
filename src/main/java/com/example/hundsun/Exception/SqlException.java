f1226090399a14d9442db373518ff4da585a35e7
xiang_buer <1813654811@qq.com>
2023-11-15 21:55:09 +0800

    git忽略log 补上缺失的sqlexception

---
+++ src/main/java/com/example/hundsun/Exception/SqlException.java
@@ -0,0 +1,20 @@
+package com.example.hundsun.Exception;
+
+public class SqlException extends java.sql.SQLException {
+    private Integer code;
+
+    public Integer getCode() {
+        return code;
+    }
+    public void setCode(Integer code){
+        this.code = code;
+    }
+    public SqlException(Integer code, String message){
+        super(message);
+        this.code = code;
+    }
+    public SqlException(Integer code, String message, Throwable cause){
+        super(message,cause);
+        this.code = code;
+    }
+}
